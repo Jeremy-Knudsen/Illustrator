83665dcaec3fa911bd17c5c31438f353c4b87b5f
Jeremy Knudsen <jeremyknudsen@outlook.com>
2017-04-12 11:47:28 -0700

    Script to rename artboards
    This script renames each artboard to its artboard number, which is
    useful for exporting to PNG images and you want to maintain the sort
    order.

---
+++ Scripts/renameArtboards.js
@@ -0,0 +1,20 @@
+// Run this script to rename all the artboards to just use the artboard number,
+// which is useful for when you export as PNG and want to maintain the order
+// for presentation purposes.
+// Jeremy Knudsen
+
+function renameArtboards() {
+    var docRef = app.activeDocument,
+        aB = docRef.artboards,
+        i = aB.length,
+        aBPrefix = "";
+    for (i; i--;) {
+        if (i < 9) {
+            aBPrefix = "0" + (i + 1) + "_";
+        } else {
+            aBPrefix = (i + 1) + "_";
+        }
+        aB[i].name = aBPrefix + aB[i].name.replace(/^(\d*_)+/, "");
+    }
+}
+renameArtboards();
